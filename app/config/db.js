6fb24a6881702419be5e7fff14225cd96eb1137d
akshay <akshay_seth@rocketmail.com>
2018-05-16 01:07:33 +0530

    commit for first live

--- app/config/db.js
+++ app/config/db.js
@@ -1,7 +1,8 @@
 module.exports = {
 //	url : 'mongodb://<user>:<pass>@mongo.onmodulus.net:27017/uw45mypu'
-    url : 'mongodb://localhost:27017/local'
+//    url : 'mongodb://localhost:27017/local'
     
-//url : 'mongodb://amitadmin:amitadmin@ds159200.mlab.com:59200/circulateitround'
+url : 'mongodb://amitadmin:amitadmin@ds159200.mlab.com:59200/circulateitround'
 //    url : 'mongodb://dbuser:dbuser@ds159200.mlab.com:59200/circulateitround'
+ //   url:'mongodb://8.30.244.139:27017/local'
 }
--- server.js
+++ server.js
@@ -1,41 +1,62 @@
-// modules =================================================
-var express        = require('express');
-//var app            = express();
-var mongoose       = require('mongoose');
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
-var app  = require('./app/custom_modules/imapp/index');
-var moment = require('moment');
-var path = require('path');
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = require('./app/custom_modules/imapp/index');
+var morgan     = require('morgan');
+var methodOverride = require('method-override')
+var _ = require('underscore');
+// configure app
 // configuration ===========================================
-global.app = app;	
-// config files
-var db = require('./config/db');
+global.app = app;
 
 
 require('./app/apis/apis')(app);
-//require('./app/routes')(app);
-require('./app/apis/admin_apis')(app);
-require('./app/apis/registration/student_registration_apis')(app);
-require('./app/apis/registration/teacher_registration_apis')(app);
-var mdb = require('./app/models/index');
-//require('./app/apis/')(app)
+require('./app/apis/result')(app);
+require('./app/apis/admin/admin_apis')(app);
+require('./app/apis/admin/registration/student_registration_apis')(app);
+require('./app/apis/admin/registration/teacher_registration_apis')(app);
+
+var mdb = require('./app/models/bear');
+app.use(morgan('dev')); // log requests to the console
+
+// configure body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080; // set our port
- mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
+
+// DATABASE SETUP
+var mongoose   = require('mongoose');
+var db = require('./app/config/db');
+mongoose.connect(db.url);
 console.log("connected db is "+db.url)
-// get all data/stuff of the body (POST) parameters
+// Handle the connection event
+var dbconn = mongoose.connection;
+dbconn.on('error', console.error.bind(console, 'connection error:'));
+
+dbconn.once('open', function() {
+  console.log("DB connection alive");
+});
+
+
+
 app.use(bodyParser.json()); // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
+// ROUTES FOR OUR API
+// =============================================================================
 
-app.use(function(req, res, next) {
+// create our router
+var router = express.Router();
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Username, SourceKey");
-
-    mdb.user.find({user_id: req.query.username}, function (err, data) {
+    /*mdb.user.find({user_id: req.query.username}, function (err, data) {
 
         if (err) {
             app.sendError(req, res, "error", err);
@@ -43,44 +64,26 @@ app.use(function(req, res, next) {
             next();
         }
         else{
-            app.sendError(req, res, "No User found with this user id!!", err);
+            app.sendError(req, res, "No User found with this user id!! Authentication", err);
         }
-    });
+    });*/
+next();
 });
+                //--------------end----------------//
+
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public/')); // set the static files location /public/img will be /img for users
 
-app.get('/test',function(req,res){
-	//console.log(moment(new Date()).format("YYYY-MM-DD"));
-//	console.log("date : "+mdbfile.dates.formatDate(new Date()));
-	app.send(req,res,[{"abc":"test"}]);
-})
 app.get('*',function(req,res){
   //res.sendfile(path.join(__dirname + '/public/views/attendance.html'));
   res.sendfile('./public/index.html');
 });
-//app.get('/viewdata/',function(req,res){
-//    console.log('m here')
-//    mdb.user.find({},function(err,data){
-//        if(err){
-//            console.log("Error found!");
-//        }
-//        else{
-////            console.log(data);
-//            res.send(data);
-//        }
-//    })
-//});
-/*app.get('/nerds',function(req,res){
-  res.sendfile('./public/views/nerd1.html');
-});*/
 
-
-// routes ==================================================
-//require('./app/routes')(app); // pass our application into our routes
-
-// start app ===============================================
-app.listen(port);	
-console.log('Magic happens on port ' + port); 			// shoutout to the user
-module.exports = app; 						// expose app
+// REGISTER OUR ROUTES -------------------------------
+/*app.use('/api', router);
+*/
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
